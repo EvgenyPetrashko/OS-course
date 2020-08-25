9ebc5f52b0ced5e0ea1909de2258d42b90a607de
EvgenyPetrashko <61521720+EvgenyPetrashko@users.noreply.github.com>
2020-08-25 16:41:19 +0300

    Update ex1.c

--- week2/ex1.c
+++ week2/ex1.c
@@ -12,5 +12,8 @@ int main() {
     printf("INT_SIZE: %ld\n", sizeof(int_var));
     printf("FLOAT_SIZE: %ld\n", sizeof(float_var));
     printf("DOUBLE_SIZE %ld\n", sizeof(double_var));
+    printf("INTEGER_MAX_VALUE: %ld\n", int_var);
+    printf("FLOAT_MAX_VALUE: %f\n", float_var);
+    printf("DOUBLE_MAX_VALUE: %lf\n", double_var);
     return 0;
 }
