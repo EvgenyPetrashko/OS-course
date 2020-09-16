c0f3510375ee8849a71adda58b13b59b5d764c2f
EvgenyPetrashko <61521720+EvgenyPetrashko@users.noreply.github.com>
2020-09-16 12:05:33 +0300

    Update ex1.c

--- week5/ex1.c
+++ week5/ex1.c
@@ -8,16 +8,21 @@
 void* foo(void* p){
     printf("THREAD: ");
     printf("%i\n", (int*)p);
+    pthread_exit(NULL);
 }
 
-int main1(void){
+int main(void){
     int n;
     scanf("%d", &n);
     pthread_t threads[n];
     int id[n];
     for (int j = 0; j <n ; ++j) {
         id[j] = j;
+        printf("Thread ");
+        printf("%d create\n", j);
         pthread_create(&threads[j],NULL, foo, &id[j]);
         pthread_join(threads[j], NULL);
+        printf("Thread ");
+        printf("%d exit\n", j);
     }
 }
