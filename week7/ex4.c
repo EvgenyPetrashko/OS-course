403f50779c4291e87af6b2c023fecda06d62e0b0
EvgenyPetrashko <61521720+EvgenyPetrashko@users.noreply.github.com>
2020-09-29 15:03:46 +0300

    Add files via upload

---
+++ week7/ex4.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void *reallocation(void *ptr, size_t initial, size_t new){
+    if (ptr == NULL){
+        return malloc(new);
+    }
+    if (new <= initial){
+        //trim the array
+        void *ptr2 = malloc(new);
+        memcpy(ptr2,ptr,new);
+        free(ptr);
+        return ptr2;
+    }else if (new == 0){
+            free(ptr);
+            return NULL;
+    } else{
+        //stretch the array
+        void *ptr2 = malloc(new);
+        memcpy(ptr2,ptr,initial);
+        free(ptr);
+        return ptr2;
+    }
+}
+
+int main(){
+    int initial = 16;
+    int final = 20;
+    int* a = (int*)calloc(initial, sizeof(int));
+    printf("Initial array:\n");
+    for (int i = 0; i < initial ; ++i) {
+        a[i] = 1;
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    a = reallocation(a,initial*sizeof(int),final*sizeof(int));
+    printf("New array:\n");
+    for (int j = 0; j < final ; ++j) {
+        printf("%d ", a[j]);
+    }
+    // 1 - is numbers from the first version of the array, random numbers - is the numbers from the reallocated array
+    free(a);
+}
